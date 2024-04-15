71be82662de711043d99251c6bc20addb79ee31c
ahmed-jobayer <jobayer320721@.com>
2024-04-16 02:59:00 +0600

    first commit

---
+++ src/Firebaase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDANvXBE1Wi33Z3-5RfI9VjQh6H6agVvw",
+  authDomain: "assignment-9-commercial-real-e.firebaseapp.com",
+  projectId: "assignment-9-commercial-real-e",
+  storageBucket: "assignment-9-commercial-real-e.appspot.com",
+  messagingSenderId: "998538247036",
+  appId: "1:998538247036:web:a697a6ca6a1663c93a42f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../../shared/Navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+          <Navbar></Navbar>
+          <h2 className="text-5xl">Its home  </h2>
+        </div>
+    );
+};
+
+export default Home;
